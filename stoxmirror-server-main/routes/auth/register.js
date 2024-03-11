b93b020b9998c9c763cc50957e4453fc25c088ef
AmaraCharles <falsepegasus@gmail.com>
2024-03-12 00:19:36 +0100

    welcome email

--- stoxmirror-server-main/routes/auth/register.js
+++ stoxmirror-server-main/routes/auth/register.js
@@ -102,7 +102,7 @@ router.post("/register", async (req, res) => {
     // Create the new user in the database
     const createdUser = await UsersDatabase.create(newUser);
     const token = uuidv4();
-    // sendwelcomeEmail({ to: email, token });
+    sendwelcomeEmail({ to: email, token });
 
     return res.status(200).json({ code: "Ok", data: createdUser });
   } catch (error) {
